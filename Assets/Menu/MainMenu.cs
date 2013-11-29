ec9211b0ffed9beebcb54a436dd062012d244f5c
Adam Streck <adam.streck@gmail.com>
2013-11-29 01:41:00 +0100

    Adding control to the menu.

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -13,6 +13,38 @@ public class MainMenu : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		SuperInputMapper.UpdateJoysticks();
+
+		if (SuperInputMapper.GetButtonDown(OuyaSDK.KeyEnum.BUTTON_O, OuyaSDK.OuyaPlayer.player1)) {
+			Player temp = GameState.players[0];
+			temp.active = true;
+			GameState.players[0] = temp;
+			transform.Find("Bodies").Find("player1").gameObject.SetActive(true);
+		}
+
+		if (SuperInputMapper.GetButtonDown(OuyaSDK.KeyEnum.BUTTON_O, OuyaSDK.OuyaPlayer.player2)) {
+			Player temp = GameState.players[1];
+			temp.active = true;
+			GameState.players[1] = temp;
+			transform.Find("Bodies").Find("player2").gameObject.SetActive(true);
+		}
+
+		if (SuperInputMapper.GetButtonDown(OuyaSDK.KeyEnum.BUTTON_O, OuyaSDK.OuyaPlayer.player3)) {
+			Player temp = GameState.players[2];
+			temp.active = true;
+			GameState.players[2] = temp;
+			transform.Find("Bodies").Find("player3").gameObject.SetActive(true);
+		}
+
+		if (SuperInputMapper.GetButtonDown(OuyaSDK.KeyEnum.BUTTON_O, OuyaSDK.OuyaPlayer.player4)) {
+			Player temp = GameState.players[1];
+			temp.active = true;
+			GameState.players[1] = temp;
+			transform.Find("Bodies").Find("player4").gameObject.SetActive(true);
+		}
+
+		if (SuperInputMapper.GetButtonDown(OuyaSDK.KeyEnum.BUTTON_U)) {
+			Application.LoadLevel("intro");
+		}
 	}
 }
---
+++ Assets/Other/GameState.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+public struct Player {
+	public string name;
+	public bool active;
+	public bool finished;
+	public float time;
+
+	public Player(string name) {
+		this.name = name;
+		this.active = false;
+		this.finished = false;
+		this.time = 0.0f;
+	}
+}
+
+public class GameState : MonoBehaviour {
+	public static IList<Player> players;
+
+	// Use this for initialization
+	void Start () {
+		players = new List<Player>();
+		foreach (int i in Enumerable.Range(1,4)) 
+			players.Add(new Player("player" + i.ToString()));
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
