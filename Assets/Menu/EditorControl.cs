47a4863b407a42d7c77cbe47123e4087c29b8b11
Adam Streck <adam.streck@gmail.com>
2014-08-03 22:30:59 +0200

    Version 1.1 finished.

--- Assets/Menu/EditorControl.cs
+++ Assets/Menu/EditorControl.cs
@@ -1,18 +1,18 @@
-﻿using UnityEngine;
-using System.Collections;
-using UnityEditor;
-
-[CustomEditor(typeof(ResetScript))]
-public class EditorControl : Editor
-{
-	public override void OnInspectorGUI()
-	{
-		DrawDefaultInspector();
-		
-		ResetScript myScript = (ResetScript)target;
-		if(GUILayout.Button("Reset stored data"))
-		{
-			myScript.Reset();
-		}
-	}
-}
+﻿//using UnityEngine;
+//using System.Collections;
+//using UnityEditor;
+//
+//[CustomEditor(typeof(ResetScript))]
+//public class EditorControl : Editor
+//{
+//	public override void OnInspectorGUI()
+//	{
+//		DrawDefaultInspector();
+//		
+//		ResetScript myScript = (ResetScript)target;
+//		if(GUILayout.Button("Reset stored data"))
+//		{
+//			myScript.Reset();
+//		}
+//	}
+//}
